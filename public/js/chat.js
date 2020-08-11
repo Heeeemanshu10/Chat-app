2beeb84af64f78e0bd212607a758b0ec1708b1a5
Himanshu Bhanpurkar <hbhanpurkar@gmail.com>
2020-08-11 17:53:07 +0530

    storing users

--- public/js/chat.js
+++ public/js/chat.js
@@ -7,13 +7,26 @@ const $sendLocationButton = document.querySelector('#send-location')
 const $messages = document.querySelector('#messages')
 
 const messageTemplate = document.querySelector('#message-template').innerHTML
+const locationMessageTemplate = document.querySelector('#location-message-template').innerHTML
+
+const { username, room } = Qs.parse(location.search, { ignoreQueryPrefix: true })
 
 socket.on('message', (message) => {
     console.log(message)
     const html = Mustache.render(messageTemplate , {
-        message
+        message: message.text,
+        createdAt: moment(message.createdAt).format('h:mm a')
     })
     $messages.insertAdjacentHTML('beforeend', html)
+})
+
+socket.on('locationMessage' , (message) => {
+    
+    const html = Mustache.render(locationMessageTemplate , {
+        url: message.url,
+        createdAt: moment(message.createdAt).format('h:mm a')
+    })
+    $messages.insertAdjacentHTML('beforeend',html)
 })
 
 $messageForm.addEventListener('submit', (e) => {
@@ -51,3 +64,5 @@ $sendLocationButton.addEventListener('click', () => {
         })
     })
 })
+
+socket.emit('join', { username, room })
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ const http = require('http')
 const express = require('express')
 const socketio = require('socket.io')
 const Filter = require('bad-words')
+const { generateMessage, generateLocationMessage } = require('./utils/messages')
+
 
 const app = express()
 const server = http.createServer(app)
@@ -14,10 +16,13 @@ const publicDirectoryPath = path.join(__dirname,'../public')
 app.use(express.static(publicDirectoryPath))
 
 io.on('connection', (socket) => {
-       
-    socket.emit('message', 'Welcome')
-    socket.broadcast.emit('message', 'A new user has joined')
+        socket.on('join' , ({ username, room }) => {
+      socket.join(room)
 
+      socket.emit('message', generateMessage('Welcome!'))
+      socket.broadcast.to(room).emit('message', generateMessage(  username +'has joined'))
+    }) 
+    
     socket.on('sendMessage' , (message , callback) => {
         const filter = new Filter()
 
@@ -25,17 +30,17 @@ io.on('connection', (socket) => {
             return callback('Profanity is not allowed')
         }
 
-        io.emit('message',message)
+        io.to('Center City').emit('message', generateMessage(message))
         callback()
     })
     
     socket.on('sendLocation', (coords,callback) => {
-        io.emit('message', 'https://google.com/maps?q='+coords.latitude+','+coords.longitude )
+        io.emit('locationMessage', generateLocationMessage('https://google.com/maps?q='+coords.latitude+','+coords.longitude ))
         callback()
     })
     
 socket.on('disconnect', () => {
-    io.emit('message', 'A user has left')
+    io.emit('message', generateMessage('A user has left'))
 })
 
 })
